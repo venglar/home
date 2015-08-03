b8c90223e8ae5f73893994d2f9ebeb1aee262613
Andrew Venglar <andrew@gosolid.net>
2015-08-02 20:15:47 -0700

    Updated page loading and navigation CSS

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,5 @@
 
 $(document).ready(function ($) {
     "use strict";
-    $('nav.main a').on('click', function(event){
-        if($('#trans-wrap').hasClass('flipped') == true){
-            $.pjax.click(event, {container: $('#wrap-front')});
-        } else {
-            $.pjax.click(event, {container: $('#wrap-back')});
-        }
-        $("#trans-wrap").toggleClass('flipped');
-    });
-
+    $(document).pjax('nav.main a', '#wrapper')
 });
